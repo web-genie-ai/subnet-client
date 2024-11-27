87508cff704819622906a7f5eb464a1fd6c6a812
sangar-1028 <sangar.work1028@gmail.com>
2024-11-27 12:50:03 -0600

    feat: add CORS middleware

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 import uvicorn
 from app.api.v1.routes.subnet import router as subnet_router
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to specify allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.get("/")
 def read_root():
